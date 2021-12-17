b7704bbc05cc803b3818bf2f0e280ab74f66ff93
egregors <root@egregors.com>
2021-12-17 20:35:54 +0300

    add Min and Max

---
+++ utils/math.go
@@ -0,0 +1,17 @@
+package utils
+
+// Max returns max Int
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// Min returns min Int
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
